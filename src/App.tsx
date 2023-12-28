42955911d479506ee1220f9b92a67720567e81e3
Jeon Eonseok <jseokeon@gmail.com>
2023-12-28 09:28:40 +0900

    init: stylelint 설정
    related to: #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import GlobalStyles from "./common/styles/GlobalStyles"
 import { ThemeProvider } from "@emotion/react"
 import {theme} from "./common/styles/theme"
+import styled from '@emotion/styled';
+
+const Text = styled.p`
+  width: 100%;
+  height: 100%;
+  border: 1rem;
+  font-size: 2rem;
+
+  &:hover {
+  width: 1010px;
+  }
+`
+
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      Go MoonShot!
+      <Text>Go MoonShot!</Text>
     </ThemeProvider>
   )
 }

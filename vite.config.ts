885fabbd0dae3dab9c21a09a71eff77132e5767c
Jeon Eonseok <jseokeon@gmail.com>
2023-12-28 13:51:49 +0900

    init: path 및 alias 설정
    related to: #3

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src/common') },
+      { find: '@apis', replacement: path.resolve(__dirname, 'src/common/apis') },
+      { find: '@assets', replacement: path.resolve(__dirname, 'src/common/assets') },
+      { find: '@components', replacement: path.resolve(__dirname, 'src/common/components') },
+      { find: '@constants', replacement: path.resolve(__dirname, 'src/common/constants') },
+      { find: '@hooks', replacement: path.resolve(__dirname, 'src/common/hooks') },
+      { find: '@styles', replacement: path.resolve(__dirname, 'src/common/styles') },
+      { find: '@type', replacement: path.resolve(__dirname, 'src/common/type') },
+      { find: '@utils', replacement: path.resolve(__dirname, 'src/common/utils') },
+    ],
+  },
 })

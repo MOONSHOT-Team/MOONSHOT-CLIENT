52d5b04a05394c5b4e7e38f4ac06bd2aaf3e013c
Jeon Eonseok <jseokeon@gmail.com>
2023-12-31 18:31:52 +0900

    init: lint-staged stylelint, prettier 검사 추가
    related to: #14

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import { Global, ThemeProvider } from "@emotion/react";
 import globalStyles from "./common/styles/globalStyles";
-import { theme } from "./common/styles/theme"
+import { theme } from "./common/styles/theme";
+import styled from "@emotion/styled";
+
+const Text = styled.div`
+  width: 100%;
+  height: 100%;
+  border: 1px;
+`;
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <Global styles={globalStyles} />
-      Go MoonShot!
+      <Text>Go MoonShot!</Text>
     </ThemeProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;

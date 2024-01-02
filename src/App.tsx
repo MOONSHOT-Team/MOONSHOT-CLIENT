76ac5ad1d5a343cb6abe1bf1b5653f2ca4a12fd2
Jun-min2 <cjm990622@naver.com>
2024-01-03 02:02:44 +0900

    fix: huskyConflict

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
 import { Global, ThemeProvider } from "@emotion/react";
 import globalStyles from "./common/styles/globalStyles";
-import { theme } from "./common/styles/theme";
-import styled from "@emotion/styled";
-
-const Text = styled.div`
-  width: 100%;
-  height: 100%;
-  border: 1px;
-`;
+import { theme } from "./common/styles/theme"
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <Global styles={globalStyles} />
-      <Text>Go MoonShot!</Text>
+      Go MoonShot!
     </ThemeProvider>
-  );
-};
+  )
+}
 
-export default App;
+export default App

581756463773a50cf51bf912c72e8529b8500368
Jeon Eonseok <jseokeon@gmail.com>
2024-01-03 14:23:18 +0900

    feat: error page 추가
    related to: #15

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,9 +1,11 @@
 import {createBrowserRouter}  from 'react-router-dom';
+import Error from '@components/Error';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>123</div>
+    element: <div>123</div>,
+    errorElement: <Error />
   }
 ])
 

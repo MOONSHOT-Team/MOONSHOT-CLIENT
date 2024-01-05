2441600d6546c4f27217d847fc5ba77b2d02e38f
sooy <101343915+SooY2@users.noreply.github.com>
2024-01-05 15:52:05 +0900

    [project] 스타일가이드 추가 (#24)
    * init: 스타일가이드 추가
    
    * init/배경색 적용
    
    * init: emotion/eslint-plugin 추가
    
    * fix: font-size 누락
    
    * refactor: font link로 가져오기
    
    * refactor: color 네이밍 수정

--- src/common/styles/globalStyles.ts
+++ src/common/styles/globalStyles.ts
@@ -1,26 +1,47 @@
 import { css } from '@emotion/react';
 import emotionNormalize from 'emotion-normalize';
 
+import { theme } from './theme';
+
 const globalStyles = css`
   ${emotionNormalize}
 
-   * {
-        box-sizing: border-box;
-        padding : 0;
-        margin: 0;
-      }
+  html {
+    font-size: 62.5%;
+  }
 
-      html{
-        font-size: 62.5%;
-      }
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+    font-family:
+      'Pretendard Variable',
+      Pretendard,
+      -apple-system,
+      BlinkMacSystemFont,
+      system-ui,
+      Roboto,
+      'Helvetica Neue',
+      'Segoe UI',
+      'Apple SD Gothic Neo',
+      'Noto Sans KR',
+      'Malgun Gothic',
+      'Apple Color Emoji',
+      'Segoe UI Emoji',
+      'Segoe UI Symbol',
+      sans-serif;
+  }
 
-      button {
-        cursor: pointer;
-        background: none;
-        border: none;
-      }
+  body {
+    color: #fff;
+    background-color: ${theme.colors.background};
+  }
 
-`
-
+  button {
+    cursor: pointer;
+    background: none;
+    border: none;
+  }
+`;
 
 export default globalStyles;

fd30d3dbad6e2e19828a11ba1a11316e46ee0c34
SooY2 <ssy67366736@gmail.com>
2024-01-06 01:25:43 +0900

    init/font setting 수정

--- src/common/styles/globalStyles.ts
+++ src/common/styles/globalStyles.ts
@@ -14,22 +14,6 @@ const globalStyles = css`
     box-sizing: border-box;
     padding: 0;
     margin: 0;
-    font-family:
-      'Pretendard Variable',
-      Pretendard,
-      -apple-system,
-      BlinkMacSystemFont,
-      system-ui,
-      Roboto,
-      'Helvetica Neue',
-      'Segoe UI',
-      'Apple SD Gothic Neo',
-      'Noto Sans KR',
-      'Malgun Gothic',
-      'Apple Color Emoji',
-      'Segoe UI Emoji',
-      'Segoe UI Symbol',
-      sans-serif;
   }
 
   body {
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import App from './App.tsx'
-import router from './Router.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+
+import App from './App.tsx';
+import router from './Router.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
     <App />
   </React.StrictMode>,
-)
+);

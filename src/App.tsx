3d8f894d5ac43cd23f884f15f9cc13fa1d1cff84
Yeonseo-Jo <jys073190@gmail.com>
2024-01-06 03:47:13 +0900

    fix: routerProvider 위치 app으로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Global, ThemeProvider } from '@emotion/react';
+import { RouterProvider } from 'react-router-dom';
 
 import globalStyles from './common/styles/globalStyles';
 import { theme } from './common/styles/theme';
+import router from './Router';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <Global styles={globalStyles} />
+      <RouterProvider router={router} />
     </ThemeProvider>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
 
 import App from './App.tsx';
-import router from './Router.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
     <App />
   </React.StrictMode>,
 );

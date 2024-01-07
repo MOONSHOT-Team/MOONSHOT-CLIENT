36ed64909333ed0abec5d80fa3dc12157e5c7ba5
Yeonseo-Jo <jys073190@gmail.com>
2024-01-07 16:29:12 +0900

    feat: 트리 ui 구현을 위한 okr mock data 상수 파일 추가

---
+++ src/common/constants/MOCK_OKR_DATA.ts
@@ -0,0 +1,33 @@
+export const MOCK_OKR_DATA = {
+  id: 0,
+  Objective: 'Objective 1',
+  KeyResults: [
+    {
+      index: 1,
+      KR: 'Key Result 1',
+      Tasks: [
+        { index: 1, Task: 'Task 1-1' },
+        { index: 2, Task: 'Task 1-2' },
+        { index: 3, Task: 'Task 1-3' },
+      ],
+    },
+    {
+      index: 2,
+      KR: 'Key Result 2',
+      Tasks: [
+        { index: 1, Task: 'Task 2-1' },
+        { index: 2, Task: 'Task 2-2' },
+        { index: 3, Task: 'Task 2-3' },
+      ],
+    },
+    {
+      index: 3,
+      KR: 'Key Result 3',
+      Tasks: [
+        { index: 1, Task: 'Task 3-1' },
+        { index: 2, Task: 'Task 3-2' },
+        { index: 3, Task: 'Task 3-3' },
+      ],
+    },
+  ],
+};

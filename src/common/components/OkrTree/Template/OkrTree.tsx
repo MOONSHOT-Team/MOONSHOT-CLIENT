ac1a120ad4afcd11bc10f3d7db8efedf2aaacaa0
Yeonseo-Jo <jys073190@gmail.com>
2024-01-08 05:29:07 +0900

    feat: okr tree 최종 template 컴포넌트 생성

---
+++ src/common/components/OkrTree/Template/OkrTree.tsx
@@ -0,0 +1,35 @@
+import styled from '@emotion/styled';
+import { IKeyResultTypes } from '@type/OkrTree/KeyResultTypes';
+
+import { ObjectiveNodes } from '../Nodes/CommonOkrNodes';
+import KrContainer from './Kr/KrContainer';
+
+interface IOkrTreeProps {
+  objTitle: string;
+  keyResultList: IKeyResultTypes[];
+}
+
+const OkrTree = ({ objTitle, keyResultList }: IOkrTreeProps) => {
+  return (
+    <StOkrTreeContainer>
+      <ObjectiveNodes title={objTitle} />
+      <StKrTreeWrapper>
+        <KrContainer keyResultsProps={keyResultList} />
+      </StKrTreeWrapper>
+    </StOkrTreeContainer>
+  );
+};
+
+export default OkrTree;
+
+const StOkrTreeContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const StKrTreeWrapper = styled.div`
+  display: flex;
+`;

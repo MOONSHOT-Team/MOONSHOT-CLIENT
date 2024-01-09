191bd286722ceb2c9ec654bc4bcc1b87eaa2df23
Yeonseo-Jo <jys073190@gmail.com>
2024-01-09 22:40:18 +0900

    feat: 알림 아이콘 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { RouterProvider } from 'react-router-dom';
 
 import globalStyles from './common/styles/globalStyles';
 import { theme } from './common/styles/theme';
+import PreviewOkrAlertMsg from './PrviewOkr/components/PreviewOkrAlertMsg';
 import router from './Router';
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
     <ThemeProvider theme={theme}>
       <Global styles={globalStyles} />
       <RouterProvider router={router} />
+      <PreviewOkrAlertMsg />
     </ThemeProvider>
   );
 };

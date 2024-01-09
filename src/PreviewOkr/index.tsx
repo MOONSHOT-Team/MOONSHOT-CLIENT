601dda56e18e640dc15c9b825743ed3dd7159075
Yeonseo-Jo <jys073190@gmail.com>
2024-01-10 04:02:20 +0900

    feat: okr 트리 위치 조정

--- src/PreviewOkr/index.tsx
+++ src/PreviewOkr/index.tsx
@@ -1,5 +1,6 @@
 import OkrTreeTemplate from '@components/OkrTree/template/OkrTreeTemplate';
 import { MOCK_OKR_DATA } from '@constants/MOCK_OKR_DATA';
+import { css } from '@emotion/react';
 
 import { PreviewKrNodes } from './components/previewOkrTreeNodes/PreviewKrNodes';
 import { PreviewTaskNodes } from './components/previewOkrTreeNodes/PreviewTaskNodes';
@@ -7,18 +8,23 @@ import { PreviewTaskNodes } from './components/previewOkrTreeNodes/PreviewTaskNo
 const PreviewOkr = () => {
   const { objTitle, krList } = MOCK_OKR_DATA;
   return (
-    // O 노드의 위치 고정을 위해 트리 가져올때 항상 상위 요소에 높이 값(100vh or 100%), 세로 가운데 정렬해야함 !
-    <div
-      style={{ display: 'flex', alignItems: 'center', height: '100vh', justifyContent: 'center' }}
-    >
+    // O 노드<의 위치 고정을 위해 트리 가져올때 항상 상위 요소에 높이 값(100vh or 100%), 세로 가운데 정렬해야함 !
+    <section css={previewOkrContainer}>
       <OkrTreeTemplate
         objTitle={objTitle}
         keyResultList={krList}
         KrNodes={PreviewKrNodes}
         TaskNodes={PreviewTaskNodes}
       />
-    </div>
+    </section>
   );
 };
 
 export default PreviewOkr;
+
+const previewOkrContainer = css`
+  display: flex;
+  align-items: center;
+  height: 100%;
+  padding-left: 26.9rem;
+`;

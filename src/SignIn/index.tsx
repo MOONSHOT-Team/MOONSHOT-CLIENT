9e47068503581886203f326ca441251baf064dfc
Jeon Eonseok <jseokeon@gmail.com>
2024-01-09 22:32:21 +0900

    chore: button type 추가
    related to: #31

--- src/SignIn/index.tsx
+++ src/SignIn/index.tsx
@@ -16,10 +16,10 @@ const SignIn = () => {
         <StSubText>
           유난한 도전을 위한 체계적인 목표 관리와 성과 트래킹, 지금 바로 문샷에서 시작해보세요.
         </StSubText>
-        <button>
+        <button type="button">
           <img src={imgKakaoLogin} alt="kakao-login-button" width={300} height={45} />
         </button>
-        <button>
+        <button type="button">
           <img src={imgGoogleLogin} alt="google-login-button" width={300} height={45} />
         </button>
       </section>

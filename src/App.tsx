d86e395d679251b1263072b98a5709d59ba7ec2c
Yeonseo-Jo <jys073190@gmail.com>
2024-01-09 22:48:58 +0900

    fix: 테스트 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { RouterProvider } from 'react-router-dom';
 
 import globalStyles from './common/styles/globalStyles';
 import { theme } from './common/styles/theme';
-import PreviewOkrAlertMsg from './PrviewOkr/components/PreviewOkrAlertMsg';
 import router from './Router';
 
 const App = () => {
@@ -11,9 +10,6 @@ const App = () => {
     <ThemeProvider theme={theme}>
       <Global styles={globalStyles} />
       <RouterProvider router={router} />
-      <div style={{ position: 'relative' }}>
-        <PreviewOkrAlertMsg />
-      </div>
     </ThemeProvider>
   );
 };

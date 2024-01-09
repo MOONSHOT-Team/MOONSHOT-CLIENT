a3ae4202ac5843145a9cff86360b0df16e38180f
Yeonseo-Jo <jys073190@gmail.com>
2024-01-09 22:45:23 +0900

    feat: 알림 아이콘 추가

--- src/PrviewOkr/components/PreviewOkrAlertMsg.tsx
+++ src/PrviewOkr/components/PreviewOkrAlertMsg.tsx
@@ -1,8 +1,13 @@
+import { IcNotice } from '@assets/icons';
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 
 const PreviewOkrAlertMsg = () => {
   return (
     <StAlertMsgBox>
+      <i css={noticeIconStyle}>
+        <IcNotice />
+      </i>
       <p>
         ‘저장하기’ 버튼을 누른 후 목표(O), Tasks에 대한 수정은 불가능하며, KR에 대한 목표 수치는
         수정이 가능합니다.
@@ -14,6 +19,9 @@ const PreviewOkrAlertMsg = () => {
 export default PreviewOkrAlertMsg;
 
 const StAlertMsgBox = styled.div`
+  display: flex;
+  gap: 1rem;
+  align-items: center;
   width: 38rem;
   height: 7.4rem;
   padding: 1.7rem 1.4rem 1.7rem 1.2rem;
@@ -22,4 +30,9 @@ const StAlertMsgBox = styled.div`
   border-radius: 8px;
 
   ${({ theme }) => theme.fonts.body_14_regular};
+`;
+
+const noticeIconStyle = css`
+  width: 2.4rem;
+  height: 2.4rem;
 `;
--- src/common/assets/icons/index.ts
+++ src/common/assets/icons/index.ts
@@ -1,3 +1,5 @@
 // import IcArrow from './IcArrow.svg?react';
-
 // export { IcArrow };
+import IcNotice from './icNotice.svg?react';
+
+export { IcNotice };

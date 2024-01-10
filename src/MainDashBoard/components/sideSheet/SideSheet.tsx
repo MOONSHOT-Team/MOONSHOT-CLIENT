aaa5a0fe37bd5da67825b823ee2a6906b56f3015
sooy <101343915+SooY2@users.noreply.github.com>
2024-01-11 00:00:26 +0900

    [mainDashboard] side sheet 체크인로그 (#71)
    * feat: 트리 ui 구현을 위한 okr mock data 상수 파일 추가
    
    * feat: 트리 node desgin
    
    * feat: okr tree line 컴포넌트 생성
    
    * feat: kr, kr-task varients template 컴포넌트 생성
    
    * fix: okr mock data api 명세서 형식으로 수정
    
    * fix: okr 관련 type 수정
    
    * fix: okr node 컴포넌트들 수정
    
    * feat: okr tree 최종 template 컴포넌트 생성
    
    * feat: tree ui 보충 구현
    
    * fix: task type undefined error 수정
    
    * feat: preview-okr 페이지 생성 후 트리 테스트
    
    * fix: 노드 추상화 없이 각자 구현 -> 스타일 파일 분리, preview 노드 분리
    
    * fix: 추상화 위해 okrTree 관련 types 수정
    
    * fix: OKR tree KrNodes, TaskNodes 컴포넌트로 받을수 있게 추상화
    
    * feat: previewOkr로 추상화 테스트
    
    * fix: taskbox label 워딩 수정
    
    * fix: first-child -> first-of-child
    
    * fix: stylelint 에러 이슈 해결
    
    * fix: stylelint overrides ts file에도 적용
    
    * fix: template 폴더, 파일명 통일
    
    * fix: kr props 네이밍 통일
    
    * fix: tree 간격 수정
    
    * fix: tree 줄어들 때 선 정렬 안맞음 문제 해결 (kr-task box 높이 고정으로)
    
    * fix: obj node stroke 색상 바꿀수 있도록 추상화
    
    * fix: 코드리뷰 반영
    
    * fix: 폴더명 수정
    
    * fix: theme color 변경
    
    * fix: 폴더명 바뀐거 커밋
    
    * feat: 체크인로그 데이터타입정의"
    
    * feat: 체크인로그 ui구현
    
    * feat/ 스크롤바 커스텀
    
    * code review: 코리 반영
    
    ---------
    
    Co-authored-by: Yeonseo-Jo <jys073190@gmail.com>
    Co-authored-by: Jun-min2 <cjm990622@naver.com>
    Co-authored-by: Yeonseo-Jo <77691829+Yeonseo-Jo@users.noreply.github.com>

--- src/MainDashBoard/components/sideSheet/SideSheet.tsx
+++ src/MainDashBoard/components/sideSheet/SideSheet.tsx
@@ -1,8 +1,10 @@
-import { keyframes } from '@emotion/react';
+import { css, keyframes } from '@emotion/react';
 import styled from '@emotion/styled';
 
 import { IcClose } from '../../assets/icons';
+import { KRDETAILDATA } from '../../constants/KrDetailData';
 import KRPeriodSelect from '../KRPeriodSelect';
+import CheckInLogs from './CheckInLogs';
 import KrStatus from './KrStatus';
 
 interface ISideSheetProps {
@@ -14,15 +16,16 @@ const SideSheet = ({ isOpen, onClose }: ISideSheetProps) => {
   return (
     <StBackground>
       <StContainer $isOpen={isOpen}>
-        <StKRDetailUpper>
+        <section css={krDetailUpperStyles}>
           <StKrDetailHeader>
             <span>kr</span>
             <span>
               <IcClose onClick={onClose} />
+              <button onClick={onClose}>x</button>
             </span>
           </StKrDetailHeader>
           <StKrTitle>통합 회원수 200,000건 돌파</StKrTitle>
-          <div>프로그래스바(공통컴포넌트)</div>
+          <div css={{ height: '3rem' }}>프로그래스바(공통컴포넌트)</div>
           <StKrStatus>
             <StKrDetailLabel>상태</StKrDetailLabel>
             <span>
@@ -34,7 +37,8 @@ const SideSheet = ({ isOpen, onClose }: ISideSheetProps) => {
             <KRPeriodSelect />
           </StKrPeriodContainer>
           <StKrCheckInBtn type="button">체크인</StKrCheckInBtn>
-        </StKRDetailUpper>
+        </section>
+        <CheckInLogs data={KRDETAILDATA.Log} />
       </StContainer>
     </StBackground>
   );
@@ -55,8 +59,10 @@ const StContainer = styled.aside<{ $isOpen: boolean }>`
   position: fixed;
   top: 0;
   right: 0;
+  display: flex;
+  flex-direction: column;
   width: 34.2rem;
-  height: 100%;
+  height: 100vh;
   background-color: ${({ theme }) => theme.colors.gray_600};
   transform: translateX(${({ $isOpen }) => ($isOpen ? '0' : '100%')});
   animation: ${({ $isOpen }) => ($isOpen ? slideIn : 'none')} 0.3s forwards;
@@ -70,11 +76,11 @@ const slideIn = keyframes`
   }
 `;
 
-const StKRDetailUpper = styled.div`
+const krDetailUpperStyles = css`
   display: flex;
   flex-direction: column;
   width: 100%;
-  padding: 2.6rem 2.2rem;
+  padding: 2.6rem 2.2rem 2.1rem;
 `;
 
 const StKrDetailHeader = styled.div`
@@ -93,12 +99,12 @@ const StKrStatus = styled.div`
   gap: 3.2rem;
   align-items: center;
   margin-top: 1.2rem;
-  margin-bottom: 1rem;
+  margin-bottom: 0.5rem;
 `;
 
 const StKrPeriodContainer = styled.div`
   display: flex;
-  gap: 2.2rem;
+  gap: 2rem;
   align-items: center;
   margin-bottom: 2.4rem;
 `;
---
+++ src/common/components/OkrTree/Template/KrTemplate/KrContainer.tsx
@@ -0,0 +1,57 @@
+import NodeLines from '@components/OkrTree/lines/NodeLines';
+import { jsx } from '@emotion/react';
+import styled from '@emotion/styled';
+import { IKeyResultTypes } from '@type/OkrTree/KeyResultTypes';
+import { ITaskNodesTypes } from '@type/OkrTree/TasksTypes';
+
+import KrTaskContainer from './KrTaskContainer';
+
+interface IKrContainerProps {
+  keyResultList: IKeyResultTypes[];
+  KrNodes: ({ idx, title }: IKeyResultTypes) => jsx.JSX.Element;
+  TaskNodes: ({ idx, title, isFirstChild }: ITaskNodesTypes) => jsx.JSX.Element;
+}
+
+const KrContainer = ({ keyResultList, KrNodes, TaskNodes }: IKrContainerProps) => {
+  return (
+    <StKrContainer>
+      {keyResultList.map((kr) => {
+        return (
+          <StKrWrapper key={kr.title}>
+            <NodeLines />
+            <KrTaskContainer krProp={kr} KrNodes={KrNodes} TaskNodes={TaskNodes} />
+          </StKrWrapper>
+        );
+      })}
+    </StKrContainer>
+  );
+};
+
+export default KrContainer;
+
+const StKrContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  & > div:first-of-type {
+    > div:first-of-type {
+      div:first-of-type {
+        background-color: transparent;
+      }
+    }
+  }
+
+  /* stylelint-disable */
+  & > div:last-child {
+    > div:first-of-type {
+      div:last-child {
+        background-color: transparent;
+      }
+    }
+  }
+`;
+
+const StKrWrapper = styled.div`
+  display: flex;
+`;

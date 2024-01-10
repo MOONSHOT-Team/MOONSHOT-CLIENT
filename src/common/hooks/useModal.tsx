12e0fa434c046a8e98fa39425bba198135127360
Jeon Eonseok <jseokeon@gmail.com>
2024-01-10 22:13:28 +0900

    feat: modal esc 키 무력화
    related to: #66

--- src/common/hooks/useModal.tsx
+++ src/common/hooks/useModal.tsx
@@ -7,6 +7,13 @@ const useModal = () => {
     modalRef.current?.showModal();
   };
 
+  // esc키 눌렀을 때 모달창 닫히는 거 막기
+  document.addEventListener('keydown', (e) => {
+    if (e.code === 'Escape') {
+      e.preventDefault();
+    }
+  });
+
   return { modalRef, handleShowModal };
 };
 

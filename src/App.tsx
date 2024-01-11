01ae3b6b7251710c845cfec2c464eb3a4978e8e2
Jeon Eonseok <jseokeon@gmail.com>
2024-01-11 22:43:53 +0900

    chore: test 코드 제거
    related to: #66

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import Modal from '@components/Modal';
 import { Global, ThemeProvider } from '@emotion/react';
-import useModal from '@hooks/useModal';
 import { RouterProvider } from 'react-router-dom';
 
 import globalStyles from './common/styles/globalStyles';
@@ -8,21 +6,10 @@ import { theme } from './common/styles/theme';
 import router from './Router';
 
 const App = () => {
-  const { modalRef, handleShowModal } = useModal();
-
   return (
     <ThemeProvider theme={theme}>
       <Global styles={globalStyles} />
       <RouterProvider router={router} />
-      <button style={{ color: 'red' }} onClick={handleShowModal}>
-        CLICK ME!!!
-      </button>
-      <Modal ref={modalRef}>
-        <p>Modal</p>
-        <form method="dialog">
-          <button style={{ color: 'red' }}>CLOSE ME</button>
-        </form>
-      </Modal>
     </ThemeProvider>
   );
 };

d5db446de831897073145b66d71513f57ff1e65a
Jun-min2 <cjm990622@naver.com>
2024-01-12 23:36:29 +0900

    feat: 드롭다운 KR UI

--- src/History/components/dropDown/HistoryList.tsx
+++ src/History/components/dropDown/HistoryList.tsx
@@ -9,7 +9,8 @@ const HistoryList = () => {
   const [dropdownVisibility, setDropdownVisibility] = useState(false);
 
   return (
-    <div className="app">
+    <div css={historyUI}>
+      <StTheYear>2023년</StTheYear>
       <StDropDownButtonContainer onClick={() => setDropdownVisibility(!dropdownVisibility)}>
         <StDropDownButtonWrapper>
           {dropdownVisibility ? <CloseDropDownIcon /> : <DropDownIcon />}
@@ -28,6 +29,26 @@ const HistoryList = () => {
           </StKeyResultWrapper>
           <div css={KrProgressBar}> 프로그래스 바 입니다.</div>
         </StDropDownKeyResult>
+        <StDropDownTask>
+          <StTaskContainer>
+            <StTaskWrapper>
+              <StKeyResult>Task 1</StKeyResult>
+              <StkeyResultText>새로운 웹사이트를 2월까지 오픈한다</StkeyResultText>
+            </StTaskWrapper>
+          </StTaskContainer>
+          <StTaskContainer>
+            <StTaskWrapper>
+              <StKeyResult>Task 1</StKeyResult>
+              <StkeyResultText>새로운 웹사이트를 2월까지 오픈한다</StkeyResultText>
+            </StTaskWrapper>
+          </StTaskContainer>
+          <StTaskContainer>
+            <StTaskWrapper>
+              <StKeyResult>Task 1</StKeyResult>
+              <StkeyResultText>새로운 웹사이트를 2월까지 오픈한다</StkeyResultText>
+            </StTaskWrapper>
+          </StTaskContainer>
+        </StDropDownTask>
         <StDropDownKeyResult>
           <StKeyResultWrapper>
             <StKeyResult>KR 2</StKeyResult>
@@ -48,7 +69,13 @@ const HistoryList = () => {
 };
 
 export default HistoryList;
-
+const historyUI = css`
+  padding: 3rem 3.6rem 0 4rem;
+`;
+const StTheYear = styled.p`
+  padding: 0 0 2.4rem 0.2rem;
+  color: ${({ theme }) => theme.colors.gray_000} ${({ theme }) => theme.fonts.title_20_semibold};
+`;
 const StDropDownButtonContainer = styled.button`
   display: flex;
   gap: 1.6rem;
@@ -96,7 +123,7 @@ const StDropDownPeriod = styled.p`
   color: ${({ theme }) => theme.colors.gray_250};
 `;
 
-const StDropDownKeyResult = styled.li`
+const StDropDownKeyResult = styled.ul`
   display: flex;
   align-items: center;
   width: 105.8rem;
@@ -131,4 +158,26 @@ const StkeyResultText = styled.p`
 const KrProgressBar = css`
   width: 27.6rem;
   margin-left: 11rem;
+`;
+
+const StDropDownTask = styled.li`
+  display: flex;
+  gap: 1.9rem;
+  align-items: center;
+  list-style-type: none;
+`;
+
+const StTaskContainer = styled.div`
+  display: flex;
+  align-items: center;
+  width: 34rem;
+  height: 4rem;
+  padding-left: 1.6rem;
+  background: ${({ theme }) => theme.colors.gray_600};
+  border-radius: 6px;
+`;
+const StTaskWrapper = styled.div`
+  display: flex;
+  gap: 1.6rem;
+  align-items: center;
 `;

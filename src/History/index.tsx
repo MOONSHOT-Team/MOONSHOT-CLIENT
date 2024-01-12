dfd1ff95d1271830f138428359a0687afbe5eb36
Jun-min2 <cjm990622@naver.com>
2024-01-12 17:54:09 +0900

    chore: 테스트 코드 삭제

--- src/History/index.tsx
+++ src/History/index.tsx
@@ -1,18 +1,5 @@
-import { css } from '@emotion/react';
-
-import ProgressBar from './components/ProgressBar';
-
 const History = () => {
-  return (
-    <div css={progressBarPosition}>
-      History
-      <ProgressBar currentProgress={40} maximumProgress={100} />
-    </div>
-  );
+  return <div>History</div>;
 };
 
 export default History;
-
-const progressBarPosition = css`
-  width: 29.8rem;
-`;

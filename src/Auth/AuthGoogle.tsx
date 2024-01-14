0160e37fc3114393a4213b43a9cb9d47d27e38f2
Jeon Eonseok <jseokeon@gmail.com>
2024-01-15 04:07:56 +0900

    feat: accessToken, refreshToken 저장
    related to: #21

--- src/Auth/AuthGoogle.tsx
+++ src/Auth/AuthGoogle.tsx
@@ -1,12 +1,25 @@
 import { fetcherPost } from '@apis/fetcher';
+import { useNavigate } from 'react-router-dom';
 import useSWR from 'swr';
 
+import { SwrType } from './type';
+
 const AuthGoogle = () => {
+  const navigate = useNavigate();
   const googleCode = new URL(window.location.href).searchParams.get('code');
 
-  const { data, error, isLoading } = useSWR('/v1/user/login', (url) =>
+  const { data, error, isLoading } = useSWR<SwrType, Error>('/v1/user/login', (url: string) =>
     fetcherPost(url, googleCode!, 'GOOGLE'),
   );
+
+  if (data) {
+    const { accessToken, refreshToken } = data.data.data.token;
+
+    localStorage.setItem('accessToken', accessToken);
+    localStorage.setItem('refreshToken', refreshToken);
+
+    navigate('/dashboard');
+  }
 
   return (
     <>

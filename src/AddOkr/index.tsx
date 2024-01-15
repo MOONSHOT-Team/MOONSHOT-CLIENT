8ac3ee04fa948d4f6a971e59771ab66200bbfb15
Yeonseo-Jo <jys073190@gmail.com>
2024-01-15 12:59:11 +0900

    feat : addKr 타이틀 부분 구현

--- src/AddOkr/index.tsx
+++ src/AddOkr/index.tsx
@@ -6,6 +6,7 @@ import KeyResultPlusCard from './components/addKr/KeyResultPlusCard';
 
 const AddOkr = () => {
   const MAX_KR_LENGTH = 3;
+  const OBJECT = '통합 회원가입수 200,000건 돌파'; // 추후 이전 단계 값 가져올 값
 
   const [clickedCard, setClickedCard] = useState<number[]>([0]);
 
@@ -49,12 +50,52 @@ const AddOkr = () => {
     );
   };
 
-  return <StAddOkrLayoutContainer>{renderKrCards()}</StAddOkrLayoutContainer>;
+  return (
+    <StAddOkrLayoutContainer>
+      <StAddOkrTitle>
+        {
+          '어떤 성과를 이루면 목표 달성에 가까워질 수 있을까요?\n목표를 측정할 수 있는 핵심 지표를 구체적으로 설정해주세요.'
+        }
+      </StAddOkrTitle>
+      <StAddOkrObjectBox>
+        <StAddOkrObjectTxt>{OBJECT}</StAddOkrObjectTxt>
+      </StAddOkrObjectBox>
+      <AddOkrCardWrapper>{renderKrCards()}</AddOkrCardWrapper>
+    </StAddOkrLayoutContainer>
+  );
 };
 
 export default AddOkr;
 
 const StAddOkrLayoutContainer = styled.section`
   display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const StAddOkrTitle = styled.h1`
+  margin: 1rem 0 0.6rem;
+  color: ${({ theme }) => theme.colors.gray_000};
+  ${({ theme }) => theme.fonts.title_20_semibold};
+
+  white-space: pre-line;
+`;
+
+const AddOkrCardWrapper = styled.div`
+  display: flex;
   gap: 2rem;
+`;
+
+const StAddOkrObjectBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.7rem 0.5rem;
+  width: fit-content;
+  margin-bottom: 3rem;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.main_purple};
+`;
+
+const StAddOkrObjectTxt = styled.h2`
+  color: ${({ theme }) => theme.colors.main_purple};
+  ${({ theme }) => theme.fonts.title_16_semibold};
 `;

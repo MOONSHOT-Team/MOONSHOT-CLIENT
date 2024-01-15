70aceedbcd6d41ffe9536bd803f4e8554506a711
Yeonseo-Jo <jys073190@gmail.com>
2024-01-16 00:04:23 +0900

    fix: 카드 공통 스타일 추출

--- src/AddOkr/components/addKr/KeyResultPlusCard.tsx
+++ src/AddOkr/components/addKr/KeyResultPlusCard.tsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
 
 import { IcPlusExtraLarge } from '../../assets';
+import { KeyResultCardStyle } from '../../styles/KeyResultCardStyle';
 
 const KeyResultPlusCard = () => {
   return (
@@ -13,21 +14,13 @@ const KeyResultPlusCard = () => {
 
 export default KeyResultPlusCard;
 
-const StKeyResultPlusCardContainer = styled.article`
-  display: flex;
-  flex-direction: column;
+const StKeyResultPlusCardContainer = styled(KeyResultCardStyle)`
   gap: 0.9rem;
   align-items: center;
   justify-content: center;
-  width: 34.7rem;
-  height: 29.8rem;
   color: ${({ theme }) => theme.colors.gray_100};
+  cursor: pointer;
   background-color: ${({ theme }) => theme.colors.gray_600};
-  border-radius: 10px;
-
-  &:hover {
-    background-color: ${({ theme }) => theme.colors.gray_450};
-  }
 
   ${({ theme }) => theme.fonts.body_10_regular};
 `;
---
+++ src/AddOkr/styles/KeyResultCardStyle.tsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const KeyResultCardStyle = styled.article`
+  display: flex;
+  flex-direction: column;
+  width: 34.7rem;
+  height: 29.8rem;
+  background-color: ${({ theme }) => theme.colors.gray_600};
+  border-radius: 10px;
+`;

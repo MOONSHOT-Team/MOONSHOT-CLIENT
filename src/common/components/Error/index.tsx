65aa33bc711885a03110f4689ef0d94b62b38270
Jeon Eonseok <jseokeon@gmail.com>
2024-01-16 17:49:59 +0900

    chore: 네이밍 컨벤션에 맞게 수정
    related to: #108

--- src/common/components/Error/index.tsx
+++ src/common/components/Error/index.tsx
@@ -17,15 +17,15 @@ const Error = () => {
           width={123}
           height={147}
         />
-        <ErrorTitle>원하시는 페이지를 찾을 수 없습니다.</ErrorTitle>
-        <ErrorText>
+        <StErrorTitle>원하시는 페이지를 찾을 수 없습니다.</StErrorTitle>
+        <StErrorText>
           찾으려는 페이지의 주소가 잘못 입력되었거나,
           <br />
           주소의 변경 혹은 삭제로 인해 사용하실 수 없습니다.
           <br />
           입력하신 페이지의 주소가 정확한지 다시 한 번 확인해주세요.
-        </ErrorText>
-        <BackToHomeLink to="/">홈페이지 돌아가기</BackToHomeLink>
+        </StErrorText>
+        <StBackToHomeLink to="/">홈페이지 돌아가기</StBackToHomeLink>
       </div>
     </>
   );
@@ -43,21 +43,21 @@ const imgBottomMargin = css`
   margin-bottom: 3.2rem;
 `;
 
-const ErrorTitle = styled.h1`
+const StErrorTitle = styled.h1`
   ${({ theme }) => theme.fonts.title_20_semibold};
 
   margin-bottom: 0.8rem;
   color: ${({ theme }) => theme.colors.gray_000};
 `;
 
-const ErrorText = styled.p`
+const StErrorText = styled.p`
   ${({ theme }) => theme.fonts.body_12_regular};
 
   margin-bottom: 4rem;
   color: ${({ theme }) => theme.colors.gray_000};
 `;
 
-const BackToHomeLink = styled(Link)`
+const StBackToHomeLink = styled(Link)`
   ${({ theme }) => theme.fonts.body_14_semibold};
 
   display: inline-flex;

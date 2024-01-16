e1ad747d9add81b0291cf036aeca8aad9b41fb64
Jeon Eonseok <jseokeon@gmail.com>
2024-01-16 14:57:20 +0900

    feat: nickname 한글 숫자 영문만 입력 받도록 구현
    related to: #112

--- src/NickName/index.tsx
+++ src/NickName/index.tsx
@@ -1,15 +1,28 @@
 import imgWordmarkWhite from '@assets/images/imgWordmarkWhite.png';
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
+import { useState } from 'react';
 
 const Nickname = () => {
+  const [nickname, setNickname] = useState('');
+
+  const handleEnteredNickname = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNickname(e.target.value.replace(/[^a-zA-Z0-9ㄱ-ㅎ가-힣]/g, ''));
+  };
+
   return (
     <section css={nicknameSection}>
       <h1 css={wordMark}>
         <img src={imgWordmarkWhite} alt="work" width={257} height={68} />
       </h1>
       <StDescriptionText>문샷에서 사용할 닉네임을 설정해 주세요.</StDescriptionText>
-      <StNicknameInput type="text" placeholder="뭉디" />
+      <StNicknameInput
+        type="text"
+        placeholder="닉네임 (특수기호, 공백 제외 7자리 이내)"
+        maxLength={7}
+        value={nickname}
+        onChange={handleEnteredNickname}
+      />
       <StStartButton>서비스 시작하기</StStartButton>
     </section>
   );

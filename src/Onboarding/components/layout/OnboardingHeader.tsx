2acd456283a882ff9942bb0f48736bc278321048
Jeon Eonseok <jseokeon@gmail.com>
2024-01-18 02:31:13 +0900

    fix: header z-index 수정
    related to: #128

--- src/Onboarding/components/layout/OnboardingHeader.tsx
+++ src/Onboarding/components/layout/OnboardingHeader.tsx
@@ -34,7 +34,7 @@ const StHeader = styled.header`
   position: fixed;
   top: 0;
   left: 0;
-  z-index: 10;
+  z-index: 30;
   display: flex;
   align-items: center;
   justify-content: space-between;

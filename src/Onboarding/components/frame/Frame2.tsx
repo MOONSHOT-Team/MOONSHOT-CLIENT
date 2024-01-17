a72ca743182c0c9a49de62382433d692ab5d2024
Jeon Eonseok <jseokeon@gmail.com>
2024-01-17 20:01:09 +0900

    design: 이미지와 텍스트 중앙 정렬
    related to: #119

--- src/Onboarding/components/frame/Frame2.tsx
+++ src/Onboarding/components/frame/Frame2.tsx
@@ -1,5 +1,6 @@
 import { css } from '@emotion/react';
 
+import imgFrame2Tree from '../../assets/imgFrame2Tree.png';
 import TextField from './TextField';
 
 const Frame2 = () => {
@@ -11,6 +12,7 @@ const Frame2 = () => {
         title="흩어져 있는 할 일을 하나의 북극성 아래"
         description="파편화된 할일들을 단 3개의 우선순위로 정렬시켜 핵심 지표를 달성하는데 집중하세요"
       />
+      <img src={imgFrame2Tree} alt="tree-img" width={986} height={576} />
     </section>
   );
 };
@@ -18,7 +20,12 @@ const Frame2 = () => {
 export default Frame2;
 
 const sectionStyle = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
   width: 100%;
   max-width: 136.6rem;
   height: 108.4rem;
+  margin: 0 auto;
 `;

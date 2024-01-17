a9cf9cbabb1e71c7192a7ed8d12a5c420ac300bc
Jeon Eonseok <jseokeon@gmail.com>
2024-01-18 02:47:15 +0900

    refactor: image pop up translate X, Y 값 건내주도록 수정
    related to: #132

--- src/Onboarding/components/frame/Frame2.tsx
+++ src/Onboarding/components/frame/Frame2.tsx
@@ -14,7 +14,14 @@ const Frame2 = () => {
         title={`흩어져 있는 할 일을\n하나의 북극성 아래`}
         description="파편화된 할일들을 단 3개의 우선순위로 정렬시켜 핵심 지표를 달성하는데 집중하세요"
       />
-      <ImgPopUp from={20} to={0} src={imgFrame2TreeGraph} alt="tree-img" width={986} height={576} />
+      <ImgPopUp
+        fromY={20}
+        toY={0}
+        src={imgFrame2TreeGraph}
+        alt="tree-img"
+        width={986}
+        height={576}
+      />
     </section>
   );
 };
--- src/Onboarding/styles/animation.ts
+++ src/Onboarding/styles/animation.ts
@@ -2,24 +2,27 @@ import { keyframes } from '@emotion/react';
 import styled from '@emotion/styled';
 
 type popUpType = {
-  from: number;
-  to: number;
+  fromX?: number;
+  fromY?: number;
+  toX?: number;
+  toY?: number;
   delay?: number;
 };
 
-const popUp = (from: number, to: number) => keyframes`
+const popUp = (fromX: number = 0, fromY: number = 0, toX: number = 0, toY: number = 0) => keyframes`
 from {
-  transform: translateY(${from}rem);
+  transform: translate(${fromX}rem, ${fromY}rem);
   opacity: 0;
 }
 
 to {
-  transform: translateY(${to}rem);
+  transform: translate(${toX}rem, ${toY}rem);
   opacity: 1;
 }
 `;
 
 export const ImgPopUp = styled.img<popUpType>`
-  animation: ${({ from, to }) => popUp(from, to)} 1s ease-out forwards;
+  border-radius: 12px;
+  animation: ${({ fromX, fromY, toX, toY }) => popUp(fromX, fromY, toX, toY)} 1s ease-out forwards;
   animation-delay: ${({ delay = 0 }) => `${delay}s`};
 `;

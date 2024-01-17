d4dac613bd281171e6367c38882236dc1e9b6c74
Jeon Eonseok <jseokeon@gmail.com>
2024-01-18 01:07:24 +0900

    refactor: section container 제거 및 common style로 분리
    related to: #127

--- src/Onboarding/components/frame/Frame2.tsx
+++ src/Onboarding/components/frame/Frame2.tsx
@@ -1,11 +1,11 @@
 import imgFrame2Tree from '../../assets/imgFrame2Tree.png';
 import { ImgPopUp } from '../../styles/animation';
-import FrameSection from './FrameSection';
+import { sectionStyle } from '../../styles/common';
 import TextField from './TextField';
 
 const Frame2 = () => {
   return (
-    <FrameSection>
+    <section css={sectionStyle}>
       <TextField
         subTitle="OKR tree"
         subTitleColor="main_purple"
@@ -13,7 +13,7 @@ const Frame2 = () => {
         description="파편화된 할일들을 단 3개의 우선순위로 정렬시켜 핵심 지표를 달성하는데 집중하세요"
       />
       <ImgPopUp from={20} to={0} src={imgFrame2Tree} alt="tree-img" width={986} height={576} />
-    </FrameSection>
+    </section>
   );
 };
 

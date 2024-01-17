81550bf2547e63c81adaedd3932884ce28b2db04
Jeon Eonseok <jseokeon@gmail.com>
2024-01-17 22:19:18 +0900

    refactor: section 부분 공통 컴포넌트로 분리
    related to: #119

--- src/Onboarding/components/frame/Frame2.tsx
+++ src/Onboarding/components/frame/Frame2.tsx
@@ -1,12 +1,11 @@
-import { css } from '@emotion/react';
-
 import imgFrame2Tree from '../../assets/imgFrame2Tree.png';
 import { imgPopUp } from '../../styles/animation';
+import FrameSection from './FrameSection';
 import TextField from './TextField';
 
 const Frame2 = () => {
   return (
-    <section css={sectionStyle}>
+    <FrameSection>
       <TextField
         subTitle="OKR tree"
         subTitleColor="main_purple"
@@ -14,19 +13,8 @@ const Frame2 = () => {
         description="파편화된 할일들을 단 3개의 우선순위로 정렬시켜 핵심 지표를 달성하는데 집중하세요"
       />
       <img css={imgPopUp} src={imgFrame2Tree} alt="tree-img" width={986} height={576} />
-    </section>
+    </FrameSection>
   );
 };
 
 export default Frame2;
-
-const sectionStyle = css`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  max-width: 136.6rem;
-  height: 108.4rem;
-  margin: 0 auto;
-`;

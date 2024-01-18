b7047f13f85ca5178326ca5c8f8c116aa6ee112b
Jeon Eonseok <jseokeon@gmail.com>
2024-01-19 01:20:14 +0900

    design: text rolling border 설정
    related to: #142

--- src/Onboarding/components/frame/Frame1.tsx
+++ src/Onboarding/components/frame/Frame1.tsx
@@ -99,6 +99,11 @@ const liftUp = keyframes`
 `;
 
 const textRolling = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
   animation: ${liftUp} 3s ease-out infinite;
 `;
 
@@ -131,7 +136,14 @@ const RollingTextBox = styled.div`
   font-weight: 500;
   line-height: 1.8rem;
   color: ${({ theme }) => theme.colors.gray_150};
-  border: 1px solid red;
+  background-image: linear-gradient(
+      ${({ theme }) => theme.colors.background},
+      ${({ theme }) => theme.colors.background}
+    ),
+    linear-gradient(87.29deg, #444 8.86%, #7165ca 55.07%, #444 91.76%);
+  background-clip: padding-box, border-box;
+  background-origin: border-box;
+  border: 1px solid transparent;
   border-radius: 24px;
 `;
 

ee079ab17f7e231e8a2bca1f9a6d0d29880f2e6f
Jun-min2 <cjm990622@naver.com>
2024-01-19 02:33:37 +0900

    fix: 마이View QA

--- src/My/constants/itemList.ts
+++ src/My/constants/itemList.ts
@@ -1,12 +1,11 @@
-import item1 from '../assets/images/item1.png';
-import item2 from '../assets/images/item2.png';
-import item3 from '../assets/images/item3.png';
-import item4 from '../assets/images/item4.png';
-import item5 from '../assets/images/item5.png';
-import item6 from '../assets/images/item6.png';
-import item7 from '../assets/images/item7.png';
-import item8 from '../assets/images/item8.png';
-
+import item1 from '../assets/images/AquiredItem/item1.png';
+import item2 from '../assets/images/AquiredItem/item2.png';
+import item3 from '../assets/images/AquiredItem/item3.png';
+import item4 from '../assets/images/AquiredItem/item4.png';
+import item5 from '../assets/images/AquiredItem/item5.png';
+import item6 from '../assets/images/AquiredItem/item6.png';
+import item7 from '../assets/images/AquiredItem/item7.png';
+import item8 from '../assets/images/AquiredItem/item8.png';
 export const ITEM_LIST = [
   { src: item1, title: 'STAR', subTitle: '유난한 도전 시작' },
   { src: item2, title: 'FIRST AID KIT', subTitle: ' 쏘아올릴 준비' },
@@ -15,5 +14,5 @@ export const ITEM_LIST = [
   { src: item5, title: 'MOON', subTitle: '북극성을 따라서' },
   { src: item6, title: 'ASTRONAUT', subTitle: '진정한 우주비행사' },
   { src: item7, title: 'TELESCOPE', subTitle: ' 탐색의 기본' },
-  { src: item8, title: 'STUFOAR', subTitle: '우주의 슈퍼스타' },
+  { src: item8, title: 'UFO', subTitle: '우주의 슈퍼스타' },
 ];

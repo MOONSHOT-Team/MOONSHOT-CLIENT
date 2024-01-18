af2fa91e47f4612669bd6a220f56e6c83a3c27dc
Jeon Eonseok <jseokeon@gmail.com>
2024-01-18 22:33:24 +0900

    feat: scrollbar 없애기
    related to: #142

--- src/Onboarding/components/layout/OnboardingLayout.tsx
+++ src/Onboarding/components/layout/OnboardingLayout.tsx
@@ -6,17 +6,27 @@ import OnboardingHeader from './OnboardingHeader';
 
 const OnboardingLayout = () => {
   return (
-    <>
+    <div css={hideScroll}>
       <OnboardingHeader />
       <main css={mainStyle}>
         <Outlet />
       </main>
       <OnboardingFooter />
-    </>
+    </div>
   );
 };
 
 export default OnboardingLayout;
+
+const hideScroll = css`
+  -ms-overflow-style: none; /* for Internet Explorer, Edge */
+  scrollbar-width: none; /* for Firefox */
+  overflow: auto;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
 
 const mainStyle = css`
   width: 100%;

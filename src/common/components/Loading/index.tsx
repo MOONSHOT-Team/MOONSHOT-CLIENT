c33c66f4a11793fa1f0b5cdb01d8774ff0e3d115
Yeonseo-Jo <jys073190@gmail.com>
2024-01-20 05:27:23 +0900

    fix: loading 수정사항 날라간거 추가

--- src/common/components/Loading/index.tsx
+++ src/common/components/Loading/index.tsx
@@ -1,13 +1,15 @@
 import animateData from '@components/Loading/assets/lottie/loading.json';
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import Lottie from 'lottie-react';
 
 const Loading = ({ isDrawer = false }: { isDrawer?: boolean }) => {
   return (
     <StLoadingContainer $isDrawer={isDrawer}>
-      <StCustomLoadingLottie animationData={animateData} loop={true}>
+      <div css={LoadingBox}>
+        <StCustomLoadingLottie animationData={animateData} loop={true} />
         <StLoadingTxt>로딩 중입니다</StLoadingTxt>
-      </StCustomLoadingLottie>
+      </div>
     </StLoadingContainer>
   );
 };
@@ -16,18 +18,23 @@ export default Loading;
 
 const StLoadingContainer = styled.section<{ $isDrawer: boolean }>`
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   width: ${({ $isDrawer }) => ($isDrawer ? 'calc(100vw - 23.2rem)' : '100vw')};
   height: 100%;
+  margin-left: auto;
+`;
+
+const LoadingBox = css`
+  display: flex;
+  flex-direction: column;
+  gap: 4rem;
+  width: 100%;
+  text-align: center;
 `;
 
 const StLoadingTxt = styled.p`
-  position: absolute;
-  top: 25rem;
-  right: 50%;
-  transform: translate(50%, 0);
-
   ${({ theme }) => theme.colors.gray_000};
 
   ${({ theme }) => theme.fonts.body_14_medium};
@@ -35,7 +42,7 @@ const StLoadingTxt = styled.p`
 
 const StCustomLoadingLottie = styled(Lottie)`
   position: relative;
-  width: 30rem;
-  height: 30rem;
+  width: 10rem;
+  height: 10rem;
   margin: 0 auto;
 `;

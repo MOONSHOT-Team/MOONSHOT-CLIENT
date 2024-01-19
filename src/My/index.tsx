6ae76636c81ce73aabeb791f108874db74bfc403
Yeonseo-Jo <jys073190@gmail.com>
2024-01-20 05:16:29 +0900

    merge: merge to develop

--- src/My/index.tsx
+++ src/My/index.tsx
@@ -1,5 +1,5 @@
 import instance from '@apis/instance';
-import Loading from '@components/Lodaing';
+import Loading from '@components/Loading';
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import { useNavigate } from 'react-router-dom';

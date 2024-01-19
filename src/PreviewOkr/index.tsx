ef6dd1a77b6fe5567af0da25607d99a396d8041f
Yeonseo-Jo <jys073190@gmail.com>
2024-01-20 03:44:27 +0900

    fix: 예외 처리 추가

--- src/PreviewOkr/index.tsx
+++ src/PreviewOkr/index.tsx
@@ -115,7 +115,7 @@ const PreviewOkr = () => {
 
     try {
       const res = await postOkrInfo('/v1/objective', finalOkrInfo);
-      console.log(finalOkrInfo, '???');
+      if (res.data.status.toString().startsWith('40')) alert('OKR 생성에 실패했습니다');
       if (res) navigate('/dashboard');
     } catch (err) {
       navigate('/error');

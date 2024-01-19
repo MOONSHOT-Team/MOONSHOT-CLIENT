27c7bca592424812bbb11fd7c6e6efcb240cb796
Jeon Eonseok <jseokeon@gmail.com>
2024-01-19 14:41:59 +0900

    design: text style 적용
    related to: #160

--- src/Onboarding/components/teamMoonshot/TeamMoonshot.tsx
+++ src/Onboarding/components/teamMoonshot/TeamMoonshot.tsx
@@ -1,4 +1,5 @@
 import { css } from '@emotion/react';
+import styled from '@emotion/styled';
 
 import imgEonseok from '../../assets/teamMoonshot/imgEonseok.png';
 import imgHyeonjin from '../../assets/teamMoonshot/imgHyeonjin.png';
@@ -17,8 +18,8 @@ const TeamMoonshot = () => {
   return (
     <section css={section}>
       <div css={textContainer}>
-        <h1>우리가 달로 향하는 방법</h1>
-        <p>일상의 혁신을 만들어가는 TEAM moonshot</p>
+        <MainTitle>우리가 달로 향하는 방법</MainTitle>
+        <SubTitle>일상의 혁신을 만들어가는 TEAM moonshot</SubTitle>
       </div>
       <Profile src={imgMinjeong} crewName="MinJeon.K" crewRole="Team Leader" />
       <Profile src={imgKyungmin} crewName="KyungMin.Y" crewRole="Product Manager" />
@@ -50,4 +51,19 @@ const section = css`
 const textContainer = css`
   width: 13.5rem;
   height: 22rem;
+`;
+
+const MainTitle = styled.h1`
+  margin-bottom: 2rem;
+  font-size: 2.4rem;
+  font-weight: 700;
+  line-height: 2.8rem;
+  color: ${({ theme }) => theme.colors.gray_000};
+`;
+
+const SubTitle = styled.p`
+  font-size: 1.2rem;
+  font-weight: 500;
+  line-height: 1.8rem;
+  color: ${({ theme }) => theme.colors.gray_250};
 `;

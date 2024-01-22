3ebcc4c111d669c1b4f1e3d7427244785be527a3
Jeon Eonseok <jseokeon@gmail.com>
2024-01-23 01:53:50 +0900

    chore: console 제거

--- src/common/apis/getNewAccessToken.ts
+++ src/common/apis/getNewAccessToken.ts
@@ -1,11 +1,8 @@
 import { signInInstance } from './instance';
 
 const getNewAccessToken = async () => {
-  console.log('is running');
-
   try {
     const REFRESH_TOKEN = localStorage.getItem('REFRESH_TOKEN');
-    console.log('refresh token', REFRESH_TOKEN);
 
     const response = await signInInstance.post(
       '/v1/user/reissue',
@@ -16,11 +13,8 @@ const getNewAccessToken = async () => {
         },
       },
     );
-    console.log('res', response);
 
     const { accessToken, refreshToken } = response.data.data;
-
-    console.log('res.data', response.data);
 
     localStorage.setItem('ACCESS_TOKEN', accessToken);
     localStorage.setItem('REFRESH_TOKEN', refreshToken);
@@ -28,7 +22,6 @@ const getNewAccessToken = async () => {
     return true;
   } catch {
     localStorage.clear();
-    console.log('what');
 
     window.location.href = '/sign-in';
 

a535ab18c55873a9dd33bff89d32a6792c72764f
Jeon Eonseok <jseokeon@gmail.com>
2024-01-23 01:37:06 +0900

    fix: refresh token 가져오는 오류 해결

--- src/common/apis/getNewAccessToken.ts
+++ src/common/apis/getNewAccessToken.ts
@@ -7,13 +7,17 @@ const getNewAccessToken = async () => {
     const REFRESH_TOKEN = localStorage.getItem('REFRESH_TOKEN');
     console.log('refresh token', REFRESH_TOKEN);
 
-    const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/v1/user/reissue`, {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization:
-          'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE3MDU5MzkzMDEsImV4cCI6MTcwNzE0ODkwMSwidXNlcklkIjoyfQ.kHmbU-lwrJY1Ktd9c_tfjHyph4p2li5H39Lh9Im2YFHYNU709TWMqioMAphI3qfGSi48ysHafHbVZB4A84uBUg',
+    const response = await axios.post(
+      `${import.meta.env.VITE_BASE_URL}/v1/user/reissue`,
+      {},
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization:
+            'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE3MDU5MzkzMDEsImV4cCI6MTcwNzE0ODkwMSwidXNlcklkIjoyfQ.kHmbU-lwrJY1Ktd9c_tfjHyph4p2li5H39Lh9Im2YFHYNU709TWMqioMAphI3qfGSi48ysHafHbVZB4A84uBUg',
+        },
       },
-    });
+    );
     console.log('res', response);
 
     const { accessToken, refreshToken } = response.data;

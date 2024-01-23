2fb2e6ccbc529eb69e240dd22d11e817c550e9df
Jeon Eonseok <jseokeon@gmail.com>
2024-01-23 18:19:36 +0900

    chore: 환경변수 type 지정

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,14 @@
 /// <reference types="vite/client" />
 /// <reference types="@emotion/react/types/css-prop" />
+
+interface ImportMetaEnv {
+  readonly VITE_BASE_URL: string;
+  readonly VITE_KAKAO_REST_API_KEY: string;
+  readonly VITE_GOOGLE_CLIENT_ID: string;
+  readonly VITE_REDIRECT_URL: string;
+  readonly VITE_GOOGLE_ANALYTICS: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}

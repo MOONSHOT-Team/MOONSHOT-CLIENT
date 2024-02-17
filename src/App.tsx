dbc1828607a88e71e7ada9c70467235c9234b81c
Jeon Eonseok <jseokeon@gmail.com>
2024-02-17 14:05:23 +0900

    chore: 주석 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import Loading from '@components/Loading';
 import { Global, ThemeProvider } from '@emotion/react';
 import { Suspense } from 'react';
-// import ReactGA from 'react-ga';
 import { RouterProvider } from 'react-router-dom';
 
 import globalStyles from './common/styles/globalStyles';
 import { theme } from './common/styles/theme';
 import router from './Router';
-
-// const gaTrackingId = import.meta.env.VITE_GOOGLE_ANALYTICS;
-// ReactGA.initialize(gaTrackingId);
 
 const App = () => {
   return (

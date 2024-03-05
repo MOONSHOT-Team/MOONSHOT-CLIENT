11d75d9cee869eb4df459dde8de6090813c03fcf
Yeonseo-Jo <jys073190@gmail.com>
2024-03-05 17:22:16 +0900

    fix: 정렬 기준 워딩 수정(달성률 -> 달성률 순

--- src/History/constants/LIST_ORDER.ts
+++ src/History/constants/LIST_ORDER.ts
@@ -3,7 +3,7 @@ interface ListOrderType {
   options: string[];
 }
 export const LIST_ORDER: ListOrderType[] = [
-  { label: '최신순', options: ['오래된 순', '달성률'] },
-  { label: '달성률', options: ['최신순', '오래된 순'] },
-  { label: '오래된 순', options: ['달성률', '최신순'] },
+  { label: '최신순', options: ['오래된 순', '달성률 순'] },
+  { label: '달성률 순', options: ['최신순', '오래된 순'] },
+  { label: '오래된 순', options: ['달성률순', '최신순'] },
 ];

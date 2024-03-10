ee9b71614bc6a691870a26d70e3a8c200ffc9db4
Yeonseo-Jo <jys073190@gmail.com>
2024-03-10 18:18:23 +0900

    fix: okrData 타입 네이밍 변경

--- src/common/type/okrTree/KeyResultTypes.ts
+++ src/common/type/okrTree/KeyResultTypes.ts
@@ -1,14 +1,5 @@
-import { ITaskTypes } from './TasksTypes';
+import { IKrListInfoTypes } from '../../../AddOkr/types/KrInfoTypes';
 
-export interface IKeyResultTypes {
+export interface IKeyResultTypes extends IKrListInfoTypes {
   keyResultId?: number;
-  // FIXME: 보낼 땐 title, 받을 땐 KeyResultTitle이라고 와 type 에러 발생 해 이를 방지하고자 둘 다 추가 추후 수정 예정
-  keyResultTitle?: string;
-  title?: string;
-  startAt?: string;
-  expireAt?: string;
-  idx: number;
-  target?: number | string;
-  metric?: string;
-  taskList?: ITaskTypes[];
 }

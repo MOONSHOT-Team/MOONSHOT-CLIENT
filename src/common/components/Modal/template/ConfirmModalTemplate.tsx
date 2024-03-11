f28da3583151edf0d16c5f8b996af56604669599
Yeonseo-Jo <jys073190@gmail.com>
2024-03-11 14:53:12 +0900

    feat: confirm 모달 공통 템플릿 컴포넌트 생성

--- src/common/components/Modal/template/ConfirmModalTemplate.tsx
+++ src/common/components/Modal/template/ConfirmModalTemplate.tsx
@@ -1,5 +1,93 @@
-const ConfirmModalTemplate = () => {
-  return <div></div>;
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+
+interface IConfirmModalTemplateProps {
+  title: string;
+  description: string;
+  cancelStr: string;
+  confirmStr: string;
+  handleClickCancel: () => void;
+  handleClickConfirm: () => void;
+}
+
+const ConfirmModalTemplate = ({
+  title,
+  description,
+  cancelStr,
+  confirmStr,
+  handleClickCancel,
+  handleClickConfirm,
+}: IConfirmModalTemplateProps) => {
+  return (
+    <StConfirmModalTemplateWrapper>
+      <div css={ConfirmTextContainer}>
+        <StConfirmModalTitle>{title}</StConfirmModalTitle>
+        <StConfirmModalDescription>{description}</StConfirmModalDescription>
+      </div>
+      <div css={ConfrimBtnContainer}>
+        <StCancelBtn onClick={handleClickCancel}>{cancelStr}</StCancelBtn>
+        <StConfirmBtn onClick={handleClickConfirm}>{confirmStr}</StConfirmBtn>
+      </div>
+    </StConfirmModalTemplateWrapper>
+  );
 };
 
 export default ConfirmModalTemplate;
+
+const StConfirmModalTemplateWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 3rem;
+  align-items: center;
+  justify-content: center;
+  width: 40rem;
+  height: 18.8rem;
+  padding: 2.4rem;
+  background-color: ${({ theme }) => theme.colors.gray_550};
+  border-radius: 10px;
+`;
+// 텍스트 영역 스타일
+const StConfirmModalTitle = styled.p`
+  color: ${({ theme }) => theme.colors.gray_000};
+  ${({ theme }) => theme.fonts.title_16_semibold};
+`;
+
+const StConfirmModalDescription = styled.p`
+  color: ${({ theme }) => theme.colors.gray_200};
+  white-space: pre-wrap;
+
+  ${({ theme }) => theme.fonts.body_14_medium};
+`;
+
+const ConfirmTextContainer = css`
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
+  text-align: center;
+`;
+
+//버튼 영역 스타일
+const ConfrimBtnContainer = css`
+  display: flex;
+  gap: 1.2rem;
+`;
+
+const StCommonBtnStyle = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 17rem;
+  height: 3.4rem;
+  color: ${({ theme }) => theme.colors.gray_000};
+  border-radius: 6px;
+
+  ${({ theme }) => theme.fonts.btn_14_semibold};
+`;
+
+const StCancelBtn = styled(StCommonBtnStyle)`
+  background-color: ${({ theme }) => theme.colors.gray_450};
+`;
+
+const StConfirmBtn = styled(StCommonBtnStyle)`
+  background-color: ${({ theme }) => theme.colors.main_darkpurple};
+`;
